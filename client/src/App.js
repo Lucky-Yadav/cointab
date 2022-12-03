12fbc541786c0ad47611dda5cc38f948e1e0a8be
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-12-03 22:29:15 +0530

    update backend or frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Private from "./components/Private";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Private>
+              {" "}
+              <Home />
+            </Private>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/Signup" element={<Signup />}></Route>
+      </Routes>
     </div>
   );
 }
